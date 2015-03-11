0e168cd3790e84913670788d33396d115a97b44c
Dafeng Guo <dafeng@strikingly.com>
2015-03-11 22:42:57 +0800

    add react-template support

--- index.js
+++ index.js
@@ -2,19 +2,30 @@ var loaderUtils = require("loader-utils");
 module.exports = function(source) {
   var hamlc, query, req, template, result;
   this.cacheable && this.cacheable(true);
-  // 
+  //
   // Requires
-  // 
+  //
   hamlc     = require("haml-coffee");
-  // 
+  //
   // Loader
-  // 
+  //
   query     = loaderUtils.parseQuery(this.query);
   req       = loaderUtils.getRemainingRequest(this).replace(/^!/, "");
-  // 
+  //
   // Compilation
-  // 
-  template  = hamlc.template(source, null, null, {placement: 'standalone', escapeHtml: false});
-  result    = "module.exports =" + template.toString();
+  //
+  // template  = hamlc.template(source, null, null, {placement: 'standalone', escapeHtml: false});
+	source = "- _ = (i18n) -> i18n # needed for i18n translation\n" + source;
+	template = hamlc.render(source, {}, {placement: 'standalone', escapeHtml: false, format: 'xhtml'});
+
+	// if it's react-template, remove quotes from attributes
+	// i.e. replace '{this.xx}' to {this.xx}
+	if (query.rt) {
+		function replaceTo(match, p1) {
+			return "{" + p1 + "}";
+		}
+		template = template.replace(/'\{([^\}]+)}'/g, replaceTo);
+	}
+  result    = "module.exports =" + template;
   return result;
 }
